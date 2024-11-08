9f6947ea292e8c90bc9884e107989cfbdda712f7
José Manuel <jmrv.uax@gmail.com>
2024-11-08 09:14:51 +0100

    Remove unnecessary html from footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,3 @@
    <footer>
      <p id="footer-text">&copy; <span id="year"></span> AI News Blog</p>
    </footer>
-   <script src="../js/scripts.js"></script>
-  </body>
-</html>
--- templates/layout.php
+++ templates/layout.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../css/styles.css">
-  <title><?php echo htmlspecialchars($title); ?></title>
-</head>
-<body>
-  <?php include 'header.php'; ?>
-  <main>
-    <?php include $content; ?>
-  </main>
-  <?php include 'footer.php'; ?>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title><?php echo htmlspecialchars($title); ?></title>
+  </head>
+  <body>
+    <?php include 'header.php'; ?>
+    <main>
+      <?php include $content; ?>
+    </main>
+    <?php include 'footer.php'; ?>
+    <script src="../js/scripts.js"></script>
+  </body>
 </html>
