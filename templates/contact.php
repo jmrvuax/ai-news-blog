1805f2b6b146cb751540f7ca0c29c8f0507adbff
José Manuel <jmrv.uax@gmail.com>
2024-11-09 11:52:28 +0100

    Add contact form

--- templates/contact.php
+++ templates/contact.php
@@ -1,5 +1,16 @@
-<main>
-    <section>
-        Contact page 
-    </section>
-</main>
+<section>
+    <h2>Contact us</h2>
+    <form id="contactForm">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" required>
+      
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required>
+      
+      <label for="message">Message:</label>
+      <textarea id="message" name="message" required></textarea>
+      
+      <button type="submit">Send</button>
+    </form>
+    <div id="thankYouMessage" style="display: none;"></div>
+  </section>
