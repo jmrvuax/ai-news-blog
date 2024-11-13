9bb5c1d00fb76c05ab952b73158ac0e80b09a9a8
José Manuel <jmrv.uax@gmail.com>
2024-11-13 14:23:37 +0100

    The title is a link to the landing page

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,7 @@
     <div class="menu-icon" id="menu-icon">
       &#9776;
     </div>
-    <h1>Welcome to AI News</h1>
+    <a href="/"><h1>Welcome to AI News</h1></a>
     <ul id="nav-menu">
       <li><a href="/">Home</a></li>
       <li><a href="/about">About</a></li>
