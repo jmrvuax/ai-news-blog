5f08d150aec181e9bb2e40044d8dde83d2a4c929
José Manuel <jmrv.uax@gmail.com>
2024-11-13 11:18:47 +0100

    Move message to the top

--- templates/contact.php
+++ templates/contact.php
@@ -1,16 +1,16 @@
 <section>
-    <h2>Contact us</h2>
-    <form id="contactForm">
-      <label for="name">Name:</label>
-      <input type="text" id="name" name="name" required>
-      
-      <label for="email">Email:</label>
-      <input type="email" id="email" name="email" required>
-      
-      <label for="message">Message:</label>
-      <textarea id="message" name="message" required></textarea>
-      
-      <button type="submit">Send</button>
-    </form>
-    <div id="thankYouMessage" style="display: none;"></div>
-  </section>
+  <h2>Contact us</h2>
+  <div id="thankYouMessage" style="display: none;"></div>
+  <form id="contactForm">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required>
+    
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
+    
+    <label for="message">Message:</label>
+    <textarea id="message" name="message" required></textarea>
+    
+    <button type="submit">Send</button>
+  </form>
+</section>
