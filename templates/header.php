7017d6cabc24a3e35519aba34575e41644238b3f
José Manuel <jmrv.uax@gmail.com>
2024-11-13 14:27:47 +0100

    Add styles to the homepage link

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,7 @@
     <div class="menu-icon" id="menu-icon">
       &#9776;
     </div>
-    <a href="/"><h1>Welcome to AI News</h1></a>
+    <a href="/" class="home-link"><h1>AI News</h1></a>
     <ul id="nav-menu">
       <li><a href="/">Home</a></li>
       <li><a href="/about">About</a></li>
