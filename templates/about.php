32627fbf67966c18e841115b038e718172ed8de8
José Manuel <jmrv.uax@gmail.com>
2024-11-13 16:41:30 +0100

    Update about

--- templates/about.php
+++ templates/about.php
@@ -1,6 +1,8 @@
 <main>
     <section>
         <h2>About this site</h2>
-        <p>This site is a simple example of a PHP application that uses the front controller pattern. The front controller is a design pattern that routes requests to the appropriate handler based on the request URI. In this example, the front controller is implemented in the index.php file, which includes the appropriate template based on the request URI.</p>
+        <p>Welcome to <b>AI News</b>, your go-to destination for the latest updates, insights, and breakthroughs in the world of Artificial Intelligence.</p>
+        <p>This blog was created with a passion for sharing cutting-edge AI research, emerging technologies, and impactful innovations shaping the future. Our mission is to make complex AI topics accessible and engaging, whether you’re a tech enthusiast, a student exploring the field, or a professional seeking to stay ahead in this fast-paced industry.</p>
+        <p>From advancements in machine learning and natural language processing to ethical discussions and industry trends, AI News covers it all.</p>
     </section>
 </main>
