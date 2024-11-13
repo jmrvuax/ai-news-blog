4fa197283ebd8bb7d909d3c03bab67c7d82366e1
José Manuel <jmrv.uax@gmail.com>
2024-11-13 17:13:34 +0100

    Remove main tag from about page

--- templates/about.php
+++ templates/about.php
@@ -1,8 +1,6 @@
-<main>
-    <section>
-        <h2>About this site</h2>
-        <p>Welcome to <b>AI News</b>, your go-to destination for the latest updates, insights, and breakthroughs in the world of Artificial Intelligence.</p>
-        <p>This blog was created with a passion for sharing cutting-edge AI research, emerging technologies, and impactful innovations shaping the future. Our mission is to make complex AI topics accessible and engaging, whether you’re a tech enthusiast, a student exploring the field, or a professional seeking to stay ahead in this fast-paced industry.</p>
-        <p>From advancements in machine learning and natural language processing to ethical discussions and industry trends, AI News covers it all.</p>
-    </section>
-</main>
+<section>
+  <h2>About this site</h2>
+  <p>Welcome to <b>AI News</b>, your go-to destination for the latest updates, insights, and breakthroughs in the world of Artificial Intelligence.</p>
+  <p>This blog was created with a passion for sharing cutting-edge AI research, emerging technologies, and impactful innovations shaping the future. Our mission is to make complex AI topics accessible and engaging, whether you’re a tech enthusiast, a student exploring the field, or a professional seeking to stay ahead in this fast-paced industry.</p>
+  <p>From advancements in machine learning and natural language processing to ethical discussions and industry trends, AI News covers it all.</p>
+</section>
