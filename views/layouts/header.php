8b2bf2f57aed9cd985f0eb4338442d7f299db59c
José Manuel <jmrv.uax@gmail.com>
2024-12-02 08:49:55 +0100

    Do not show Contact link to authenticated user

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -7,7 +7,9 @@
     <ul id="nav-menu">
       <li><a href="/">Home</a></li>
       <li><a href="/about">About</a></li>
-      <li><a href="/contact">Contact</a></li>
+      <?php if (!isset($_SESSION['user'])): ?>
+        <li><a href="/contact">Contact</a></li>
+      <?php endif; ?>
       <?php if (isset($_SESSION['user'])): ?>
         <li><a href="/messages">Messages</a></li>
         <li><a href="/logout">Logout</a></li>
