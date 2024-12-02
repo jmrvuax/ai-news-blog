984137344a734e30bf535216e812b254e972905c
José Manuel <jmrv.uax@gmail.com>
2024-12-02 08:57:29 +0100

    Move backToMessages link to the top

--- views/contact/message.php
+++ views/contact/message.php
@@ -1,8 +1,8 @@
 <section>
+    <a href="/messages">Back to Messages</a>
     <h2>Message from <?php echo htmlspecialchars($message['name']); ?></h2>
     <p><strong>Email:</strong> <?php echo htmlspecialchars($message['email']); ?></p>
     <p><strong>Date Sent:</strong> <?php echo htmlspecialchars($message['created_at']); ?></p>
     <h3>Message:</h3>
     <p><?php echo nl2br(htmlspecialchars($message['message'])); ?></p>
-    <a href="/messages">Back to Messages</a>
 </section>
